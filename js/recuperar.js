54214121035002053cb269469796918b7d0cec14
shyy guyy <65934801+wladimir12312312321312312@users.noreply.github.com>
2022-06-06 01:03:46 -0400

    Registro contrasena
    me rindo no pude hacerlo funcionar en la base de datos, solo con el localstorage

---
+++ js/recuperar.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    $('#contact-submit').on('click', function(e) {
+        e.preventDefault();
+        if (!$("#contact")[0].checkValidity()) {
+          $("#contact")[0].reportValidity();
+          return;
+        }
+let dataStorage= JSON.parse(localStorage.getItem("usuario"));
+console.log(dataStorage)
+
+dataStorage.pass_usuario= document.getElementById("pass").value;
+
+localStorage.setItem("usuario",JSON.stringify(dataStorage))
+
+
+
+
+
+        var nom_usuario=$("#nom_usuario").val();
+        console.log(nom_usuario);
+
+        var ape_usuario=$("#ape_usuario").val();
+        console.log(ape_usuario);
+
+        var rut_usuario=$("#rut_usuario").val();
+        console.log(rut_usuario);
+
+        var mail_usuario=$("#mail_usuario").val();
+        console.log(mail_usuario);
+
+        var tel_usuario=$("#tel_usuario").val();
+        console.log(tel_usuario);
+
+        var pass_usuario=$("#pass_usuario").val();
+        console.log(pass_usuario);
+
+        var settings = {
+            "url": "http://localhost:3000/usuarios",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+              "rut_usuario": rut_usuario,
+              "nom_usuario": nom_usuario,
+              "ape_usuario": ape_usuario,
+              "tel_usuario": tel_usuario,
+              "mail_usuario": mail_usuario,
+              "pass_usuario": pass_usuario
+            }),
+          };
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+            alert("usuario creado correctamente")
+            window.location="login.html"
+          });
+    }); 
+});
+
+
+
+
+
+function cambiarPass(){
+
+let dataStorage= JSON.parse(localStorage.getItem("usuario"));
+console.log(dataStorage)
+
+dataStorage.pass_usuario= document.getElementById("pass").value;
+
+localStorage.setItem("usuario",JSON.stringify(dataStorage))
+
+
+
+}
